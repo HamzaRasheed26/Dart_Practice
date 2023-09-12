0347070d12f79a58ca694b2d3c5083db5a0c23d9
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2023-09-12 22:42:17 +0500

    Solve Ex 5

---
+++ Week 01/EX 5.dart
@@ -0,0 +1,18 @@
+// Take two lists, for example:
+// a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+// b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// and write a program that returns a list that contains only the elements that are
+// common between them (without duplicates). Make sure your program works on two lists of different sizes.
+
+void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  List<int> common = [];
+
+  for (int i = 0; i < a.length; i++) {
+    if (b.contains(a[i]) && !common.contains(a[i])) {
+      common.add(a[i]);
+    }
+  }
+  print(common);
+}
